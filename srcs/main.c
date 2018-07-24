454185c082368cc4538e800bca9c6ce261eb1671
Coralie Eugene <baeth@MacBook-Pro-de-Coralie.local>
2018-07-24 13:21:10 +0200

    voila

--- srcs/main.c
+++ srcs/main.c
@@ -12,8 +12,33 @@
 
 #include "RTv1.h"
 
-int		main(void)
+void		ft_fail(char *str, t_data *data)
 {
+	ft_putstr_fd(str, 2);
+	ft_putchar('\n');
+	ft_putstr_fd("State: ", 2);
+	ft_putstr_fd("rtv1 is now quitting due to an error.\n", 2);
+	if (data)
+		exit(0);
+	exit(0);
+}
+
+t_data		*new_list(void)
+{
+	t_data	*new;
+
+	if (!(new = (t_data *)malloc(sizeof(t_data) * 1)))
+		ft_fail("Error: Could not allocate memory", NULL);
+	new->img = NULL;
+	new->obj = NULL;
+	return (new);
+}
+
+int			main(void)
+{
+	t_data	*data;
+
+	data = new_list();
 	ft_putstr("ca fonctionne.\n");
 	return (0);
 }
